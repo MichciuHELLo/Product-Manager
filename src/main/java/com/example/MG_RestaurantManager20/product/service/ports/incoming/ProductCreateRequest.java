f6cc6b1983ce6703087a315db4755e2c690b838a
MichciuHELLo <michcio113@gmail.com>
2022-04-30 14:58:18 +0200

    feat: add ProductCreateRequest and change of endPoints

---
+++ src/main/java/com/example/MG_RestaurantManager20/product/service/ports/incoming/ProductCreateRequest.java
@@ -0,0 +1,2 @@
+package com.example.MG_RestaurantManager20.product.service.ports.incoming;public class ProductCreateRequest {
+}
---
+++ src/test/java/com/example/MG_RestaurantManager20/IntegrationTests/products/adapters/web/api/ProductCreateRequestIntegrationTest.java
@@ -0,0 +1,2 @@
+package com.example.MG_RestaurantManager20.IntegrationTests.products.adapters.web.api;public class ProductCreateRequestIntegrationTest {
+}
